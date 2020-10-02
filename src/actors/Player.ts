fa5dd142a658aad3a2a477641a7b120227b56dc8
rpetkov123 <r.valentinov@casualino.com>
2020-10-02 09:41:13 +0300

    Modified assets; updated player spritesheet

--- src/actors/Player.ts
+++ src/actors/Player.ts
@@ -14,9 +14,16 @@ class Player extends BaseActor {
         this.movementSpeed = this.baseSpeed;
         this.hitPoints = this.baseHP;
 
+        this.scene.textures.addSpriteSheetFromAtlas("player", {
+            frameWidth: 64,
+            frameHeight: 64,
+            atlas: "spaceShooter",
+            frame: "ship_player"
+        });
+
         this.scene.anims.create({
             key: "player_idle",
-            frames: this.scene.anims.generateFrameNames("ship_player"),
+            frames: this.scene.anims.generateFrameNames("player"),
             frameRate: 20,
             repeat: -1
         });
--- src/scene/Preload.ts
+++ src/scene/Preload.ts
@@ -41,7 +41,7 @@ class Preload extends Phaser.Scene {
         //images
         this.load.image("background", "assets/images/background.png");
         this.load.atlas("foreground", "assets/images/foreground.png", "assets/images/foreground.json");
-        this.load.spritesheet("ship_player", "assets/images/ship_player.png", { frameWidth: 64, frameHeight: 64 });
+        this.load.atlas("spaceShooter", "assets/images/spaceShooter.png", "assets/images/spaceShooter.json");
 
         //audio
         this.load.audio("laser1", ["assets/audio/laser1.ogg", "assets/audio/laser1.mp3", "assets/audio/laser1.m4a"]);
