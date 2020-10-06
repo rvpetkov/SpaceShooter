0c8dca06c5350611027e755e4fdaf9c4dacc04d9
rpetkov123 <r.valentinov@casualino.com>
2020-10-06 11:41:50 +0300

    Removed test sound from SingleLaserBullet

--- src/weapons/SingleLaserBullet.ts
+++ src/weapons/SingleLaserBullet.ts
@@ -3,20 +3,15 @@ import { LaserBullet } from "./bullets/LaserBullet";
 import { Weapon } from "./Weapon";
 
 class SingleLaserWeapon extends Weapon {
-    private sound: Phaser.Sound.BaseSound;
-
     constructor(scene: Phaser.Scene) {
         super(scene, LaserBullet);
 
         this.fireRate = 200;
-
-        this.sound = this.scene.sound.add("laser1");
     }
 
     public shoot(direction: DirectionType, x: number, y: number): void {
             let bullet: LaserBullet = this.getFirstDead(true);
             if (bullet) {
-                this.sound.play();
                 bullet.fire(direction, x, y);
             }
     }
